f4840c3380c37845a9d2e478aab34b0d5482756f
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-08 21:28:25 -0700

    TASKS 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point 
+ * 
+ * Description: Using the main function 
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	
+	printf ("size of a char: % 1d bytes(s)\n", size of (c));
+	printf ("size of an int: % 1d bytes(s)\n", size of (i));
+	printf ("size of a float: % 1d bytes(s)\n", size of (f));
+       return(0);
+}       
