83f0ee07a0e7c2c57e83b82267016278d41d3e68
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-08 20:26:23 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * msin - print the string in the put function 
+ *
+ * Description: using the main function 
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(vold)
+{
+	print f ("with proper grammer, but the outcome is a piece of art,\n");
+}
