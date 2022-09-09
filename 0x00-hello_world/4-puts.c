d4805600c5bd0ee959043da320c1977fcbd34ac3
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-08 19:49:20 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main(void)
+int main(vold)
 {
 	puts("\"programming is like building a multilingual puzzled");
 	return (0);
