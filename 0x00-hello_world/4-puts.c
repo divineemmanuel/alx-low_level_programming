d2862941b2efe23a748aeb7adeed6550497a7bec
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-08 19:28:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  *
  * Description: using the main function 
- * this program prints "programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
-	puts ("\" programming is like building a multilingual puzzled");
+	puts("\" programming is like building a multilingual puzzled");
 	return (0);
 }
