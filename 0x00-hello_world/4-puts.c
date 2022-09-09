85592a5a18542dc889c4c7120007222a4dd9d82a
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-08 22:12:59 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-#include <stdio.h>
 
-/*
- *  main - printing "Programming is like building a multilingual puzzle"
- *
- * Return: 0 Always (Successful Printing)
- */
+
+
+/**
+
+* main -Entry point
+
+*
+
+*  Return : Always 0 (Success)
+
+*/
+
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
