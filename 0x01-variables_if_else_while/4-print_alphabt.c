490f9e492dcf350dddd03df1d51eb8ce7c9070de
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-11 05:57:25 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+        for (ch = 'a'; low <= 'z'; low++)
+
+	{	
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+	}
+	        putchar('\n');
+	} 
+}
+	return (0);
+
+}
