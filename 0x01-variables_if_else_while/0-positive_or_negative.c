d1e5e1ab26ab0213b5e0af7e31486cd5aa05f671
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-11 15:26:37 -0700

    print alphabt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,10 @@ int main(void)
 		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n); 
+	{
 	else if (n < 0)
 		printf("%d is negative\n", n);
 
 	return (0);
-	{
+}
