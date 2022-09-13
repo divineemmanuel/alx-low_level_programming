c6b48ea90a1069c8e761f873eb1852cfb1bce3de
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-13 16:12:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet -- prints a-z
+ *
+ * Description: 1-alphabet.c - prints the alphabet in lowercase followed by
+ * aline
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
