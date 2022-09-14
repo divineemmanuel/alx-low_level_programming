ed5152210e537bc2174e478954dcead6faad73a0
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-13 19:51:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,11 +5,9 @@
  * _isalpha - checks for alphabetic characters
  *
  * Description: checks for alpha
- *
  * Return: 1 - alphabetic character 0 - otherwise
  *
  * @c:input.
- *
  */
 int _isalpha(int c)
 {
@@ -18,8 +16,7 @@ int _isalpha(int c)
 		return (1);
 	}
 	else
-	}
+	{
 		return (0);
 	}
-
 }
