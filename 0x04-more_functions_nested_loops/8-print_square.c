cd871f89d9346e96eff0443e594330cef12abbeb
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-20 09:41:03 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#include <stdio.h>
->>>>>>> dce35b9076fa90b064d9ba4e16d579e28221e88e
+
 /**
  * print_square - Prints a squareusing the character #.
  * @size: The size of the square.
