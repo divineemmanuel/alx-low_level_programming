ec9aa982594cb0fdae750b0e6afa5da6e5bf477e
divineemmanuel <divineemmanuel361@gmail.com>
2022-09-23 15:47:24 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,22 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - puts a char
- *
- * Return: the character to write to stdout.
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ * Return: returns poiner to @dest
  */
 
-int _putchar(char c)
+char *_strcat(char *dest, char *src)
 {
-	return(write(1, &c, 1));
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
